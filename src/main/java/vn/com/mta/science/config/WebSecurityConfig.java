eb418dce540a68fea53716bda00582f67f014c18
lanhuong <huong.nguyen@itechcorp.com.vn>
2020-09-19 22:24:05 +0700

    05

--- src/main/java/vn/com/mta/science/config/WebSecurityConfig.java
+++ src/main/java/vn/com/mta/science/config/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable()
+        http.cors().configurationSource(corsConfigurationSource()).and()
                 .csrf().disable()
                 .httpBasic();
     }
